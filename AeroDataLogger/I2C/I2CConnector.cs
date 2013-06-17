63fb359d03a64d053748233c0fabf242285f99f9
Chris Brook <chrisbrook83@users.noreply.github.com>
2013-06-17 23:26:58 +0100

    Rename solution and restructure files

---
+++ AeroDataLogger/I2C/I2CConnector.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+
+namespace AeroDataLogger.I2C
+{
+    /// <summary>
+    /// Helper class for working with the I2C bus.
+    /// </summary>
+    public class I2CConnector
+    {
+        private I2CDevice.Configuration _config;
+        private I2CDevice _device;
+
+        private const int TIMEOUT_MS = 1000;
+        
+        public I2CConnector(byte deviceAddress, int clockRateKHz)
+        {
+            _config = new I2CDevice.Configuration(deviceAddress, clockRateKHz);
+            _device = new I2CDevice(_config);
+        }
+
+        public int Write(byte[] writeBuffer)
+        {
+            I2CDevice.I2CTransaction[] writeTransaction = new I2CDevice.I2CTransaction[]
+            { 
+                I2CDevice.CreateWriteTransaction(writeBuffer)
+            };
+
+            int written = _device.Execute(writeTransaction, TIMEOUT_MS);
+
+            if (written == 0)
+            {
+                Debug.Print("No data could be sent to the module."); 
+            }
+
+            return written;
+        }
+        
+        public int Read(byte[] resultBuffer)
+        {
+            var transactions = new I2CDevice.I2CTransaction[]
+            {
+                I2CDevice.CreateReadTransaction(resultBuffer)
+            };
+
+            int read = _device.Execute(transactions, TIMEOUT_MS);
+
+            if (read == 0)
+            {
+                Debug.Print("Data could not be read from the module.");
+            }
+
+            return read;
+        }
+    }
+}
---
+++ AeroDataLogger/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+ Developed from the code sample written by Johannes Paul Langner
+ http://meineweltinmeinemkopf.blogspot.co.uk/2013/01/mpu6050-sensor-auslesen-mit-dem-netduino.html
+ */
+
+using System.Threading;
+using AeroDataLogger.MPU6050;
+using Microsoft.SPOT;
+
+namespace AeroDataLogger
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            MPU6050Device mpu6050 = new MPU6050Device();
+
+            AccelerationAndGyroData sensorResult;
+            while (true)
+            {
+                sensorResult = mpu6050.GetSensorData();
+                Debug.Print(sensorResult.ToString());
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
